afed3f60733a7c889393a8d22d82b2c9fd18f8a6
aaditya1499 <aadityashrivastava@outlook.com>
2019-10-05 00:24:20 +0530

    Content updated.

--- assets/js/script.js
+++ assets/js/script.js
@@ -145,15 +145,14 @@ $(document).ready(function() {
 // google map
 ///////////////////////////////
 
-function initialize() {
+function initMap() {
   var mapProp = {
-    center: new google.maps.LatLng(51.508742, -0.12085),
-    zoom: 5,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    center: new google.maps.LatLng(22.6904064, 75.8669724),
+    zoom: 18.75,
     disableDefaultUI: true,
     scrollwheel: false
   };
   var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 }
 
-google.maps.event.addDomListener(window, "load", initialize);
+google.maps.event.addDomListener(window, "load", initMap);
