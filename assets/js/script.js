b971b5691fcc525d935d63a332f3c22e0cfcc278
aaditya1499 <aadityashrivastava@outlook.com>
2019-10-05 00:51:25 +0530

    Added loader and corrected footer mobile view.

--- assets/js/script.js
+++ assets/js/script.js
@@ -132,6 +132,18 @@ document.onscroll = scroll;
 // Testimonial Slide
 ///////////////////////////////
 
+$(window).load(function() {
+
+  // will first fade out the loading animation 
+  $("#loader").fadeOut("slow", function(){
+
+    // will fade out the whole DIV that covers the website.
+    $("#preloader").delay(300).fadeOut("slow");
+
+  });       
+
+})
+
 $(document).ready(function() {
   $("#testimonial-container").owlCarousel({
     navigation: false, // Show next and prev buttons
